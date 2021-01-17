b125b77758330a445ad5e2359bbfcde3e3d931de
tryrealty <72769711+tryrealty@users.noreply.github.com>
2021-01-17 22:40:05 +0500

    upp

--- zepher/js/main.js
+++ zepher/js/main.js
@@ -31,6 +31,8 @@ document.querySelector("#close-modal").addEventListener('click', function() {
 
 
 
+
+
 // target elements with the "draggable" class
 interact('#child2')
   .draggable({
@@ -56,13 +58,52 @@ interact('#child2')
 
       // call this function on every dragend event
       end (event) {
-        var textEl = event.target.querySelector('p')
+        // var textEl = event.target.querySelector('p')
 
-        textEl && (textEl.textContent =
-          'moved a distance of ' +
-          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                     Math.pow(event.pageY - event.y0, 2) | 0))
-            .toFixed(2) + 'px')
+        // textEl && (textEl.textContent =
+          // 'moved a distance of ' +
+          // (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     // Math.pow(event.pageY - event.y0, 2) | 0))
+            // .toFixed(2) + 'px')
+      }
+    }
+  })
+  
+  
+  
+
+// target elements with the "draggable" class
+interact('#team-child2')
+  .draggable({
+    // enable inertial throwing
+	startAxis: 'x',
+	lockAxis: 'x',
+	
+  
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        // var textEl = event.target.querySelector('p')
+
+        // textEl && (textEl.textContent =
+          // 'moved a distance of ' +
+          // (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     // Math.pow(event.pageY - event.y0, 2) | 0))
+            // .toFixed(2) + 'px')
       }
     }
   })
