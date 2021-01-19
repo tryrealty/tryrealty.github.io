12137a952315c4c57e847355a43ff918878e910a
tryrealty <72769711+tryrealty@users.noreply.github.com>
2021-01-19 18:28:05 +0500

    up

--- zepher/mail.php
+++ zepher/mail.php
@@ -29,13 +29,13 @@
 
 
 
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "sdv.mikhail@gmail.com, msdv@pm.me";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-// echo "Thank You!";
-?>
+// <?php $name = $_POST['name'];
+// $email = $_POST['email'];
+// $message = $_POST['message'];
+// $formcontent="From: $name \n Message: $message";
+// $recipient = "sdv.mikhail@gmail.com, msdv@pm.me";
+// $subject = "Contact Form";
+// $mailheader = "From: $email \r\n";
+// mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+// // echo "Thank You!";
+// ?>
